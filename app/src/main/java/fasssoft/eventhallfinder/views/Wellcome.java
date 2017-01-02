a87c1e52a9e9744661008cec7d8c56764a9dcac8
sultan arshad <suul77@gmail.com>
2017-01-02 22:01:33 +0500

    commit  5.1  final (sqlite,brodRec,sharPref,fragment,crud,pojo,volley,restApiLaravel) runnig test=ok

--- app/src/main/java/fasssoft/eventhallfinder/views/Wellcome.java
+++ app/src/main/java/fasssoft/eventhallfinder/views/Wellcome.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -13,6 +14,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,6 +38,11 @@ public class Wellcome extends AppCompatActivity {
     Button btnShowWel, btnWellAdd;
     ArrayList<DatumHallDetail> datumHallDetailsList;
     HallDetailPojo hallDetailPojo;
+    //ads
+
+    private InterstitialAd mInterstitialAd;
+    private TextView mLevelTextView;
+    private AdView mBannerAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +52,22 @@ public class Wellcome extends AppCompatActivity {
         btnWellAdd = (Button) findViewById(R.id.btnWelAdd);
         datumHallDetailsList = new ArrayList<>();
         hallDetailPojo = new HallDetailPojo();
+        ////ads
+        // Create the next level button, which tries to show an interstitial when clicked.
+        //   mLevelTwoButton = ((Button) findViewById(R.id.level_two_button));
+        // Load the add into Admob banner view.
+        mBannerAd = (AdView) findViewById(R.id.banner_AdView);
+        // Text view to show the level number.
+        mLevelTextView = (TextView) findViewById(R.id.level_text_view);
+        // Creating and load a  new InterstitialAd .
+       // mInterstitialAd = createNewIntAd();
+       // loadIntAdd();
+        //Load BannerAd
+        showBannerAd();
+        btnWellAdd.setEnabled(true);
+
+
+        //ADS end
 
         btnShowWel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,4 +85,13 @@ public class Wellcome extends AppCompatActivity {
             }
         });
     }
+
+    //adds
+    private void showBannerAd() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("75C0210003DEC7504E626A3A2CCB2A67")
+                .build();
+        mBannerAd.loadAd(adRequest);
+    }
+    //ends
 }
