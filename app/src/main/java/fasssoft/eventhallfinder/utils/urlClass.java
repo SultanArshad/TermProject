a43be0584a2ceb4764b6e22f1e72145e6433afd3
sultan arshad <suul77@gmail.com>
2017-01-05 01:38:59 +0500

    commit  6.0  final (sqlite,brodRec,services,sharPref,fragment,volley,crud,restApiLaravel) runnig test=ok

--- app/src/main/java/fasssoft/eventhallfinder/utils/urlClass.java
+++ app/src/main/java/fasssoft/eventhallfinder/utils/urlClass.java
@@ -6,7 +6,7 @@ package fasssoft.eventhallfinder.utils;
 
 public class urlClass {
 
-    public static final String url = "http://192.168.10.6/api/";
+    public static final String url = "http://192.168.43.203/api/";
     public static final String apilogintests = url + "logintests";
     public static final String apihalldetails = url + "halldetails/";
 }
--- app/src/main/java/fasssoft/eventhallfinder/views/LoginSinup/signup.java
+++ app/src/main/java/fasssoft/eventhallfinder/views/LoginSinup/signup.java
@@ -1,52 +1,72 @@
 package fasssoft.eventhallfinder.views.LoginSinup;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.mock.MockPackageManager;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import fasssoft.eventhallfinder.R;
-
+import fasssoft.eventhallfinder.Services.GPSTracker;
 import fasssoft.eventhallfinder.models.Database.DatabaseHelper;
 import fasssoft.eventhallfinder.models.User;
 import fasssoft.eventhallfinder.utils.urlClass;
 import fasssoft.eventhallfinder.views.Wellcome;
 import fasssoft.eventhallfinder.views.main;
 
+import static android.support.v7.mediarouter.R.id.time;
+
 public class signup extends AppCompatActivity {
-    ImageView sback;
+    ImageView sback, myLoc;
     EditText etEmailSup, etPassSup, etHallNameSup, etFnameSup, etLocationSup;
     TextView sup;
     String pName, pEmail, pLocation, pHall, pPass;
     DatabaseHelper helper = new DatabaseHelper(this);
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
 
+    GPSTracker gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         sback = (ImageView) findViewById(R.id.sback);
+        myLoc = (ImageView) findViewById(R.id.myLoc);
         etEmailSup = (EditText) findViewById(R.id.etEmailSup);
         etFnameSup = (EditText) findViewById(R.id.etFnameSup);
         etHallNameSup = (EditText) findViewById(R.id.etHallNameSup);
         etPassSup = (EditText) findViewById(R.id.EtPassSup);
         etLocationSup = (EditText) findViewById(R.id.etLocationSup);
         sup = (TextView) findViewById(R.id.sup);
+
+        //permission
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission)
+                    != MockPackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+                        REQUEST_CODE_PERMISSION);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ///permission ends
+
 
         sback.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,6 +128,31 @@ public class signup extends AppCompatActivity {
 
             }
         });///onclik end
+        ///location start
+        myLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+// create class object
+                gps = new GPSTracker(signup.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+etLocationSup.setText("Latitude("+latitude+") Longitude("+longitude+")");
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+
+            }
+        });
+        ///end location
     }
 }
